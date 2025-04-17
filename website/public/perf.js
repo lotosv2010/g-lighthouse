0343c6022ee0b4c85ff7fed1c9bcdfb09d719bf3
Robin <lotosv2010@163.com>
2025-04-17 16:45:17 +0800

    feat(fid): 实现FID的监听

--- website/public/perf.js
+++ website/public/perf.js
@@ -14,6 +14,7 @@
     FP: 0,  // 首次绘制
     FCP: 0, // 首次内容绘制
     LCP: 0, // 首次布局绘制
+    FID: 0, // 首次输入延迟
   }
 
   // FP & FCP: 如果观察者观察到了指定类型的性能条目，就执行回调
@@ -44,4 +45,14 @@
     });
     observer.disconnect();
   }).observe({ type: 'largest-contentful-paint', buffered: true })
+  // FID
+  new PerformanceObserver((entryList, observer) => {
+    const entries = entryList.getEntries();
+    entries.forEach(entry => {
+      // 首次用户交互，开始处理的时间 减去 开始交互的时间，就是首次交互延迟的时间
+      data.FID = entry.processingStart - entry.startTime;
+      console.log('记录首次交互延迟(FID)', data.FID);
+    });
+    observer.disconnect();
+  }).observe({ type: 'first-input', buffered: true })
 });
